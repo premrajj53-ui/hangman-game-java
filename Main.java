24d96be0e42bac4c392e054b01ef96082a149cfe
prem <premrajj53@gmail.com>
2026-03-15 11:59:51 +0530

    added Word state and length of word

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String [] args){
+        String word = " pizza";
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Character> WordState = new ArrayList<>();
+        int wrongGuiess = 0 ;
+
+for (int i=0; i <word.length(); i++)
+{
+    WordState.add('_');
+}
+sc.close();
+    }
+}
