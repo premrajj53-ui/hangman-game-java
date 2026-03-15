30ae04b5dc07fa0d611d25677b6789a657d18a33
prem <premrajj53@gmail.com>
2026-03-15 12:23:04 +0530

    made hangman art method for each wrong guiess

--- Main.java
+++ Main.java
@@ -13,6 +13,40 @@ for (int i=0; i <word.length(); i++)
     WordState.add('_');
 }
         System.out.println("Welcome to java hangman game!");
+        System.out.println(getHangman(5));
 sc.close();
+    }
+    static String getHangman(int wrongGuiess){
+        return switch (wrongGuiess){
+            case 0 -> """
+                    
+                    """;
+            case 1 -> """
+                    O
+                    
+                    """;
+            case 2 -> """
+                    O
+                   / \\
+                    """;
+            case 3 -> """
+                    O
+                   / \\
+                    |
+                    """;
+            case 4 -> """
+                    O
+                   / \\
+                    |
+                   /
+                    """;
+            case 5 -> """
+                     O
+                    / \\
+                     |
+                    / \\
+                    """;
+            default ->  "" ;
+        };
     }
 }
